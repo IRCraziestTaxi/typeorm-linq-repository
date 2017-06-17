d105fb3118974130928bbebb8fc142016ac09cd6
Who Cares <it@doesnt.matter>
2017-06-17 18:40:07 -0500

    Forgot index.ts.

---
+++ index.ts
@@ -0,0 +1,4 @@
+export { IComparableQuery } from './query/interfaces/IComparableQuery';
+export { IQuery } from "./query/interfaces/IQuery";
+export { IRepositoryBase } from "./repository/interfaces/IRepositoryBase";
+export { RepositoryBase } from "./repository/RepositoryBase";
